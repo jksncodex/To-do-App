976a5f6fed6b520898a0cd74049b4e68623b8ad2
Jay <jacksoncodex@gmail.com>
2026-06-13 23:23:18 +0100

    create basic GUI without interactions, day 16

---
+++ gui.py
@@ -0,0 +1,12 @@
+import FreeSimpleGUI
+
+import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+window = FreeSimpleGUI.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
